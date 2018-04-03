65e5abd14433505acbdc2e5376f05e2f6eb4c7ea
Brian LeBlanc <bleblan2@unb.ca>
2018-04-03 14:10:07 -0300

    Remove loading spinner from dashboard map

--- src/components/Dashboard/Dashboard.container.js
+++ src/components/Dashboard/Dashboard.container.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { compose, withProps } from 'recompose';
 import { withScriptjs, withGoogleMap } from 'react-google-maps';
-import { RefreshIndicator } from 'material-ui';
 import config from '../../config';
 import adventures from '../../data/adventures.json';
 import Dashboard from './Dashboard.presentation';
@@ -10,15 +9,7 @@ export default compose(
   withProps({
     googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${config.GOOGLE_MAPS_API_KEY}&` +
       'v=3.exp&libraries=geometry,drawing,places',
-    loadingElement: (
-      <RefreshIndicator
-        className="Loading"
-        size={50}
-        top={100}
-        left={0}
-        loadingColor="#FF9800"
-        status="loading" />
-    ),
+    loadingElement: <div />,
     containerElement: <div className="MapContainer" />,
     mapElement: <div className="MapElement" />
   }),
