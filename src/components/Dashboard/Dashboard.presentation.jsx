fcd54d6417df0fa00607710f62f8d2906d7068b3
Sam Westfield <samwest07@gmail.com>
2018-04-06 00:16:34 -0300

    Gets user's location. Displays icon on map. Error with Proptypes

--- src/components/Dashboard/Dashboard.presentation.jsx
+++ src/components/Dashboard/Dashboard.presentation.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { GoogleMap } from 'react-google-maps';
+import { GoogleMap, Marker } from 'react-google-maps';
+import { geolocated, geoPropTypes } from 'react-geolocated';
 import MarkerWithInfo from './MarkerWithInfo.presentation';
 import './Dashboard.css';
 
@@ -8,30 +9,64 @@ class Dashboard extends Component {
   render() {
     const { adventures } = this.props;
 
-    return (
-      <div className="MapWrapper">
-        <GoogleMap
-          defaultZoom={7}
-          defaultCenter={{ lat: 46.542009, lng: -66.234974 }} >
-          {adventures.map((adventure) => {
-            let location = adventure.location;
+    const currentLocationIcon = {
+      path: 'M-6,0a6,6 0 6,0 12,0a6,6 0 1,0 -12,0',
+      fillColor: '#2c00fa',
+      fillOpacity: 1,
+      scale: 1,
+      strokeColor: '#2c00fa',
+      strokeWeight: 12,
+      strokeOpacity: 0.2
+    };
 
-            if (location && location.latitude && location.longitude) {
-              return (
-                <MarkerWithInfo
-                  key={adventure.id}
-                  adventure={adventure} />
-              );
+    return (
+      <div>
+        <div className="MapWrapper">
+          <GoogleMap
+            defaultZoom={7}
+            defaultCenter={{ lat: 46.542009, lng: -66.234974 }} >
+            {!this.props.isGeolocationAvailable
+              ? <div>Your browser does not support Geolocation</div>
+              : !this.props.isGeolocationEnabled
+                ? <div>Geolocation is not enabled</div>
+                : this.props.coords
+                  ? <Marker
+                    position={{
+                      lat: this.props.coords.latitude,
+                      lng: this.props.coords.longitude
+                    }}
+                    icon= {currentLocationIcon}
+                  />
+                  : <div />
             }
-          })}
-        </GoogleMap>
+
+            {adventures.map((adventure) => {
+              let location = adventure.location;
+
+              if (location && location.latitude && location.longitude) {
+                return (
+                  <MarkerWithInfo
+                    key={adventure.id}
+                    adventure={adventure} />
+                );
+              }
+            })}
+          </GoogleMap>
+        </div>
       </div>
     );
   }
 }
 
 Dashboard.propTypes = {
-  adventures: PropTypes.array.isRequired
+  adventures: PropTypes.array.isRequired,
+  coords: {...Dashboard.propTypes, ...geoPropTypes}
 };
 
-export default Dashboard;
+
+export default geolocated({
+  positionOptions: {
+    enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(Dashboard);
